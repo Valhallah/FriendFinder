77fd9541d6c4bc63279346ba731710c1f9a00ede
ValHall <Valynhall@gmail.com>
2017-04-30 15:00:09 -0400

    logic fix

--- routing/api-routes.js
+++ routing/api-routes.js
@@ -7,43 +7,48 @@ module.exports = function(app) {
         return res.json(friends);
     });
 
-    app.post('/api/friends', function(req, res){
+    app.post('/api/friends', function(req, res) {
 
-		var match = {
-			name: "",
-			image: "",
-			matchDiff: 100
-		};
-		var usrData 	= req.body;
-		var usrName 	= usrData.name;
-		var usrImage 	= usrData.image;
-		var usrScores 	= usrData.scores;
+  		var match = {
+  			name: "nomatch",
+  			image: "nomatch",
+  			matchDiff: 1000
+  		};
 
-		var totalDiff = 0;
+  		var usrData 	= req.body;
+  		var usrName 	= usrData.name;
+  		var usrImage 	= usrData.image;
+  		var usrScores 	= usrData.scores;
 
-		//loop through the friends data array of objects to get each friends scores
-		for(var i = 0; i < [friends].length-1; i++){
-			console.log(friends[i].name);
-			totalDiff = 0;
+  		// for each friend
+  		for(var friendNum = 0; friendNum < friends.length; friendNum++){
+        // this friend
+        var friendVar = friends[friendNum];
+        // total difference for this friend
+        var totalDiff = 0;
+        // for each score
+        for (var scoreNum = 0; scoreNum < usrScores.length; scoreNum++) {
+          // calculate difference of this score
+          var difference = usrScores[scoreNum] - friendVar.scores[scoreNum];
+          // get absolute value
+          var absoluteDifference = Math.abs(difference);
+          // add to current difference for this friend
+          totalDiff = totalDiff + absoluteDifference;
+        }
 
-			for(var j = 0; j < 10; j++){
+        // if this friend is a closer match, update the match to this friend
+        if (totalDiff < match.matchDiff) {
+          match.name = friendVar.name;
+          match.image = friendVar.photo;
+          match.matchDiff = totalDiff;
+        }
+  		}
 
-				totalDiff += Math.abs(parseInt(usrScores[j]) - parseInt(friends[i].scores[j]));
 
-				if (totalDiff <= match.friendDiff){
+      // save this new friend
+  		friends.push(usrData);
 
-	
-					match.name = friends[i].name;
-					match.photo = friends[i].photo;
-					match.matchDiff = totalDiff;
-				}
-			}
-		}
-
-		friends.push(usrData);
-
-		res.json(match);
-
+  		res.json(match);
 
 
     });
